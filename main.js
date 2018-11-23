2cde75d3326230a85266e260916c56bce4890276
sixiaofeng <sxf754872493@gmail.com>
2018-11-23 21:44:30 +0800

    aboutMe

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-$('.nav').on('click','li',(e)=>{
-  $(e.currentTarget).find('.nav-item').addClass('active').parent().siblings().find('.nav-item.active').removeClass('active')
+ScrollReveal().reveal('.pictureWrapper',{
+  delay: 0,
+  useDelay: 'onload',
+  reset: true,
+  distance: '50px',
+  duration: 500 ,
+  easing: 'ease'
 })
+ScrollReveal().reveal('.selfIntroduction', { 
+  delay: 300,
+  useDelay: 'onload',
+  reset: true,
+  distance: '50px',
+  duration: 500 ,
+  easing: 'ease'
+})
+$('.nav').on('click','li',(e)=>{
+  e.preventDefault()
+  $(e.currentTarget).find('.nav-item').addClass('active').parent().siblings().find('.nav-item.active').removeClass('active')
+  let id = $(e.currentTarget).find('span').attr('id')
+  let index = $(e.currentTarget).index()
+  let element = $('main')[`${index}`]
+  if(!$(element).hasClass('visible')){
+    $(element).addClass('visible')
+      .siblings('.visible').removeClass('visible')
+  }
+})
+
+
